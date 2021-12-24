bf1b5c575c6d23ccd604f5b4bbaad7afcbaf4ad7
mubeen iqbal <mubeen.iqbal@devsinc.com>
2021-12-24 22:40:20 +0500

    Smtp of google in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true)\
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['username'],
+    password:             ENV['password'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+  config.action_mailer.default_url_options = { host: 'mughees-butt.herokuapp.com', protocol: 'https' }
 
 end
