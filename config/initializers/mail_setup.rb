0a4d3feaf63cbeb7978a1897af826726f5274e16
mubeen iqbal <mubeen.iqbal@devsinc.com>
2021-12-24 22:09:08 +0500

    Code Refactor

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 ActionMailer::Base.smtp_settings = {
-  address: Rails.application.credentials.mailer[:address],
-  port: Rails.application.credentials.mailer[:port],
-  domain: Rails.application.credentials.mailer[:domain],
-  user_name: Rails.application.credentials.mailer[:user_name],
-  password: Rails.application.credentials.mailer[:password],
+  address: ENV['address'],
+  port: ENV['port'],
+  domain: ENV['domain'],
+  user_name: ENV['user_name'],
+  password: ENV['password'],
   authentication: 'plain',
   enable_starttls_auto: true
 }
